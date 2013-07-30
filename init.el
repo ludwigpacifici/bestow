b1818ef47f42275178165908dac65d128007dd9a
Ludwig PACIFICI <ludwig@lud.cc>
2013-07-30 11:21:14 +0200

    complete init.el

--- init.el
+++ init.el
@@ -1,20 +1,28 @@
+;;;;;;;;;;;;;;
+;; packages ;;
+;;;;;;;;;;;;;;
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq url-proxy-services '(("no_proxy" . "work\\.com")
+                           ("http" . "proxy:3128")))
 (package-initialize)
 
-(defvar my-packages '(clojure-mode
-                      clojure-test-mode
-                      
+(defvar my-packages '(auto-complete
+                      clojure-mode
+                      clojure-test-mode                      
                       nrepl
                       paredit
-                      solarized-theme))
+                      solarized-theme
+                      yasnippet))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-;; coding look
+;;;;;;;;;;;;;;;;;
+;; coding look ;;
+;;;;;;;;;;;;;;;;;
 (load-theme 'solarized-dark t)
 (set-face-attribute 'default nil :font "Monaco" :height 113)
 (global-hl-line-mode t)
@@ -23,13 +31,17 @@
 (show-paren-mode t)
 (set-cursor-color "red")
 
-;; gui
+;;;;;;;;;
+;; gui ;;
+;;;;;;;;;
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (setq inhibit-splash-screen t)
 
-;; behavior
+;;;;;;;;;;;;;;
+;; behavior ;;
+;;;;;;;;;;;;;;
 (windmove-default-keybindings 'meta) ;; easy switching between visible buffers
 (ido-mode t)
 (iswitchb-mode t)
@@ -39,12 +51,36 @@
 (fset 'yes-or-no-p 'y-or-n-p) ;; yes-no shortcuts
 (delete-selection-mode t) ;; delete selected text
 (setq x-stretch-cursor t) ;; cursor as wide as the character it is over
+(electric-pair-mode t) ;; handle open/close brackets
+(yas-global-mode t) ;; yasnippet
+;; auto complete mod
+;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;; set the trigger key so that it can work together with yasnippet on tab key,
+;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
 
-;; coding style
+;;;;;;;;;;;;;;;;;;
+;; coding style ;;
+;;;;;;;;;;;;;;;;;;
 (setq require-final-newline 't) ;; newline at the end of the file
 (setq-default indent-tabs-mode nil) ;; no tabs
+(setq tab-width 4) ;; tabs size
 
-;; Character encoding:
+;;;;;;;;;;;;;;;
+;; C++ style ;;
+;;;;;;;;;;;;;;;
+(setq-default c-basic-offset 3) ;; indent size
+(setq c-default-style "k&r") ;; indent style
+(add-to-list 'auto-mode-alist '("\\.r\\'" . c-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Character encoding ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
 ;; utf-8 / input-method
 (setq locale-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
