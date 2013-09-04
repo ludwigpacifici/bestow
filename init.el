49a364eb1b45022a0c9f3573c03682c7095bf79d
Ludwig PACIFICI <ludwig@lud.cc>
2013-09-04 17:56:56 +0200

    simplifications

--- init.el
+++ init.el
@@ -29,14 +29,14 @@
 ;;;;;;;;;;;;;;;;;
 ;; coding look ;;
 ;;;;;;;;;;;;;;;;;
-(set-background-color "#fdf6e3")
+(set-background-color "#eee8d5")
 (set-face-attribute 'default nil :font "Monaco" :height 115)
 (global-hl-line-mode t)
 (setq line-number-mode t)
 (setq column-number-mode t)
 (show-paren-mode t)
 (set-cursor-color "blue")
-(setq-default cursor-type 'bar)
+(setq-default cursor-type 'box)
 (blink-cursor-mode 0) ;; no blinking cursor
 
 ;;;;;;;;;
@@ -65,10 +65,8 @@
 (delete-selection-mode t) ;; delete selected text
 (setq x-stretch-cursor t) ;; cursor as wide as the character it is over
 (electric-pair-mode t) ;; handle open/close brackets
-(require 'uniquify) ;; overrides Emacs’ default mechanism for making buffer names unique
-(setq uniquify-buffer-name-style 'forward)
-(require 'saveplace) ;; When you visit a file, point goes to the last place where it was when you previously visited the same file
-(setq-default save-place t)
+(setq uniquify-buffer-name-style 'forward) ;; overrides Emacs’ default mechanism for making buffer names unique
+(setq-default save-place t) ;; When you visit a file, point goes to the last place where it was when you previously visited the same file
 (setq visible-bell nil)
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -104,17 +102,6 @@
 (prefer-coding-system 'utf-8)
 (set-language-environment "UTF-8") ;; prefer utf-8 for language settings
 (set-input-method nil) ;; no funky input for normal editing
-
-;;;;;;;;;;;;;;;;;
-;; match paren ;;
-;;;;;;;;;;;;;;;;;
-(global-set-key [(control \])] 'match-paren)
-(defun match-paren (arg)
-  "Go to the matching paren if on a paren; otherwise insert %."
-  (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-        (t (self-insert-command (or arg 1)))))
 
 ;;;;;;;;;;;
 ;; Modes ;;
