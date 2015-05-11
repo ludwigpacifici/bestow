75b634584e062272ea186050b4a345b75eb23de1
Ludwig PACIFICI <ludwig@lud.cc>
2015-05-10 20:58:30 -0400

    cleaning unused var

--- init.el
+++ init.el
@@ -128,9 +128,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
