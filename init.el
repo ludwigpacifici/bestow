a43cec7f9b80bb0edfdc6ded3aac2585d5e85176
Ludwig PACIFICI <ludwig@lud.cc>
2015-06-01 22:16:34 +0100

    magit startup option disabled

--- init.el
+++ init.el
@@ -120,6 +120,8 @@
 (load "~/.emacs.d/clang-format.el")
 (global-set-key [C-M-tab] 'clang-format-region)
 
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 ;;;;;;;;;;;;;;;
 ;; shortcuts ;;
 ;;;;;;;;;;;;;;;
@@ -138,18 +140,3 @@
 (load-file "~/.emacs.d/cplusplus.el")
 (load-file "~/.emacs.d/python-as-ide.el")
 (load-file "~/.emacs.d/gnu-global.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
