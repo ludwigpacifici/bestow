0f354f501e27290905b6123089df0b3ff2030763
Ludwig PACIFICI <ludwig@lud.cc>
2015-06-02 23:05:04 +0100

    update pyth0n

--- python-as-ide.el
+++ python-as-ide.el
@@ -1,11 +1,6 @@
 ;; information form: https://github.com/jhamrick/emacs/blob/master/.emacs.d/settings/python-settings.el
 
 (require 'python)
-(require 'pydoc)
-(require 'pydoc-info)
-
-(setq-default py-shell-name "/usr/local/bin/ipython3")
-(setq python-shell-interpreter "/usr/local/bin/ipython3")
 
 (eval-after-load 'python
   '(define-key python-mode-map (kbd "C-c !") 'python-shell-switch-to-shell))
