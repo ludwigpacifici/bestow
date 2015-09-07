b4af757df4c69f1140df595248014f5d095a94e5
Ludwig PACIFICI <ludwig@lud.cc>
2015-09-07 22:17:51 +0100

    Correct some predicate about gui options

--- init-appearance.el
+++ init-appearance.el
@@ -1,6 +1,8 @@
 (when (display-graphic-p)
   (load-theme 'monokai t)
-  (global-hl-line-mode t)) ;; Highlight current line
+  (global-hl-line-mode t)
+  (tool-bar-mode 0)
+  (scroll-bar-mode -1))
 
 (if (eq system-type 'darwin)
     (progn
@@ -9,7 +11,5 @@
   (set-face-attribute 'default nil :family "Courier 10 Pitch")
   (set-face-attribute 'default nil :height 130))
 
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode -1)
 (set-cursor-color "gold")
+(menu-bar-mode 0)
