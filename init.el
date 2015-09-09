2213cadf024af9476b19965c814e14c0f64d70b1
Ludwig PACIFICI <ludwig@lud.cc>
2015-09-09 15:15:17 +0100

    No shortcut to kill emacs

--- init.el
+++ init.el
@@ -99,6 +99,11 @@
 
 (setq tramp-default-method "ssh")
 
+(defun dont-kill-emacs ()
+  (interactive)
+  (error (substitute-command-keys "To exit emacs: \\[kill-emacs]")))
+(global-set-key "\C-x\C-c" 'dont-kill-emacs)
+
 ;;;;;;;;;;;;;;;
 ;; shortcuts ;;
 ;;;;;;;;;;;;;;;
