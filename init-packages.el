00580fc7ee431a0de44d7b01e5f9286136b1fed2
Ludwig PACIFICI <ludwig@lud.cc>
2015-11-13 08:56:42 +0000

    Package Sources Are HTTPS Only

--- init-packages.el
+++ init-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (defvar my-packages '(
