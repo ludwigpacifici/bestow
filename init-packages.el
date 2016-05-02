4593568eea201a1b657c013332cda04f6ebafe9f
Ludwig PACIFICI <ludwig@lud.cc>
2016-05-02 15:25:15 +0100

    updated melpa

--- init-packages.el
+++ init-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (defvar my-packages '(
