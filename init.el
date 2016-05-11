03ff5a275cea3b5630fad724fc74a381309faead
Ludwig PACIFICI <ludwig@lud.cc>
2016-05-11 23:20:57 +0100

    Use use-package

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;; init.el --- Ludwig's Emacs configuration
+
+;;; Code:
+
 ;; Garbage collector kicks in when 100Mb is used
 (setq gc-cons-threshold 100000000)
 
@@ -9,85 +13,177 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(defvar my-packages '(
-                      cider
-                      clang-format
-                      clojure-mode
-                      cmake-font-lock
-                      d-mode
-                      exec-path-from-shell
-                      expand-region
-                      flx
-                      flx-ido
-                      ggtags
-                      ido-grid-mode
-                      ido-ubiquitous
-                      magit
-                      markdown-mode
-                      monokai-theme
-                      paredit
-                      rainbow-delimiters
-                      rainbow-mode
-                      scss-mode
-                      smex
-                      solarized-theme
-                      typit
-                      yaml-mode
-                      zenburn-theme
-                      ))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(require 'use-package)
+(setq use-package-verbose t)
 
 (setq user-mail-address "ludwig@lud.cc"
       user-full-name "Ludwig PACIFICI")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Mac Os X environment variables ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-(when (eq system-type 'darwin)
-  (setq mac-command-modifier 'meta))
-
-;;;;;;;;;;;;;;
-;; behavior ;;
-;;;;;;;;;;;;;;
-(setq x-stretch-cursor t) ;; Wide cursor on tabs
-(setq ring-bell-function 'ignore) ;; Turn off alarms
-(line-number-mode t)
-(column-number-mode t)
-(setq-default cursor-type 'box)
-(blink-cursor-mode -1)
-(setq initial-scratch-message "")
-(setq inhibit-splash-screen t) ;; No splash screen
-(setq frame-title-format "%b - Emacs") ;; Buffer name in the title bar
-(setq icon-title-format "%b - Emacs") ;; Buffer name in the title bar
-(delete-selection-mode t) ;; Delete selected text
-(setq mode-require-final-newline t)
-(autoload 'ibuffer "ibuffer" "List buffers." t)
-(setq auto-save-timeout 60) ;; Autosave every minute
-(savehist-mode t) ;; Save minibuffer historic
-(setq load-prefer-newer t)
-(toggle-frame-fullscreen)
-(setq current-language-environment "English")
-(delete-selection-mode t)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+(autoload 'ibuffer "ibuffer" "List buffers." t)
+(blink-cursor-mode -1)
+(column-number-mode t)
+(delete-selection-mode t)
+(delete-selection-mode t) ;; Delete selected text
 (global-auto-revert-mode t) ;; Auto refresh buffers when edits occur outside emacs
-(show-paren-mode t)
-(setq scroll-preserve-screen-position t)
-(size-indication-mode 0)
-
-(when (display-graphic-p)
-  (load-theme 'solarized-dark t)
-  (global-hl-line-mode t)
-  (tool-bar-mode 0)
-  (scroll-bar-mode -1))
-
-(set-cursor-color "gold")
+(global-hl-line-mode t)
+(line-number-mode t)
 (menu-bar-mode 0)
+(savehist-mode t) ;; Save minibuffer historic
+(scroll-bar-mode -1)
+(setq auto-save-timeout 60) ;; Autosave every minute
+(setq current-language-environment "English")
+(setq inhibit-splash-screen t)
+(setq initial-scratch-message "")
+(setq load-prefer-newer t)
+(setq mode-require-final-newline t)
+(setq ring-bell-function 'ignore) ;; Turn off alarms
+(setq scroll-preserve-screen-position t)
+(setq tramp-default-method "ssh")
+(setq x-stretch-cursor t) ;; Wide cursor on tabs
+(setq-default cursor-type 'box)
+(show-paren-mode t)
+(size-indication-mode 0)
+(toggle-frame-fullscreen)
+(tool-bar-mode 0)
+
+(use-package cider
+  :ensure t
+  :config
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
+
+(use-package clang-format
+  :ensure t
+  :bind ("C-M-<tab>" . clang-format-region))
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'subword-mode)
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
+
+(use-package cmake-font-lock
+  :ensure t
+  :config
+  (add-hook 'cmake-mode-hook #'cmake-font-lock-activate))
+
+(use-package d-mode
+  :ensure t)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package ggtags
+  :ensure t
+  :bind
+  ("M-," . pop-tag-mark)
+  :config
+  (add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1)))))
+
+(use-package ido
+  :ensure t
+  :config
+  (setq ido-auto-merge-work-directories-length -1
+        ido-create-new-buffer 'always
+        ido-default-file-method 'selected-window
+        ido-enable-flex-matching t
+        ido-use-filename-at-point 'guess)
+  (ido-mode t)
+  (ido-everywhere t))
+
+(use-package ido-ubiquitous
+  :ensure t
+  :config
+  (ido-ubiquitous-mode +1))
+
+(use-package ido-grid-mode
+  :ensure t
+  :config
+  (setq ido-grid-mode-first-line nil
+        ido-grid-mode-max-rows 3
+        ido-grid-mode-min-rows 3)
+  (ido-grid-mode +1))
+
+(use-package flx-ido
+  :ensure t
+  :config
+  (flx-ido-mode +1)
+  ;; disable ido faces to see flx highlights
+  (setq ido-use-faces nil))
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package paredit
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'paredit-mode)
+  (add-hook 'ielm-mode-hook #'paredit-mode)
+  (add-hook 'lisp-mode-hook #'paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+(use-package rainbow-mode
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-mode))
+
+(use-package saveplace
+  :config
+  (setq-default save-place t))
+
+(use-package scss-mode
+  :ensure t)
+
+(use-package smex
+  :ensure t
+  :bind ("M-x" . smex))
+
+(use-package solarized-theme
+  :ensure t
+  :config
+  (load-theme 'solarized-dark t))
+
+(use-package typit
+  :ensure t)
+
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-separator "/")
+  ;; rename after killing uniquified
+  (setq uniquify-after-kill-buffer-p t)
+  ;; don't muck with special buffers
+  (setq uniquify-ignore-buffers-re "^\\*"))
+
+(use-package yaml-mode
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;
 ;; coding style ;;
@@ -104,15 +200,6 @@
 (setq-default c-basic-offset 2)
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
-;;;;;;;;;;
-;; Tags ;;
-;;;;;;;;;;
-(require 'ggtags)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-              (ggtags-mode 1))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Character encoding ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
@@ -124,71 +211,10 @@
 (set-language-environment "UTF-8") ;; prefer utf-8 for language settings
 (set-selection-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
-(when (display-graphic-p)
-  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
-
-;;;;;;;;;;;;;
-;; Modules ;;
-;;;;;;;;;;;;;
-(require 'ido)
-(ido-mode t)
-(ido-everywhere t)
-(setq ido-auto-merge-work-directories-length -1
-      ido-enable-flex-matching t
-      ido-use-faces nil)
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
-(require 'smex)
-(smex-initialize)
-(require 'flx-ido)
-(flx-ido-mode 1)
-(ido-grid-mode 1)
-(setq ido-grid-mode-first-line nil
-      ido-grid-mode-max-rows 3
-      ido-grid-mode-min-rows 3)
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward) ;; Overrides Emacs’ default mechanism for making buffer names unique
-
-(require 'saveplace)
-(setq-default save-place t)
-
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-(require 'expand-region)
-
-(autoload 'markdown-mode "markdown-mode" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-(autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
-(add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode))
-
-(autoload 'cmake-font-lock-activate "cmake-font-lock" nil t)
-(add-hook 'cmake-mode-hook 'cmake-font-lock-activate)
-
-(setq tramp-default-method "ssh")
 
 (defun dont-kill-emacs ()
   (interactive)
   (error (substitute-command-keys "To exit emacs: \\[kill-emacs]")))
-
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
-(add-hook 'prog-mode-hook #'rainbow-mode)
-
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook #'enable-paredit-mode)
-(add-hook 'clojure-mode-hook #'paredit-mode)
-(add-hook 'cider-repl-mode-hook #'paredit-mode)
 
 ;;;;;;;;;;;;;;;
 ;; shortcuts ;;
@@ -196,7 +222,6 @@
 (global-set-key "\C-cc" 'compile)
 (global-set-key "\C-x\C-c" 'dont-kill-emacs)
 (global-set-key "\M- " 'hippie-expand)
-(global-set-key (kbd "C-=") 'er/expand-region)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 (global-set-key (kbd "C-c <left>")  'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
@@ -204,10 +229,8 @@
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key [C-M-tab] 'clang-format-region)
-(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
 (global-set-key [f11] 'toggle-frame-fullscreen)
 (global-unset-key (kbd "C-z"))
 (windmove-default-keybindings 'meta) ;; Move point from window to window
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta))
