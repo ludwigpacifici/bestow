b8e5f9af1c215a5334aba4b716ef19a45f288565
Lud <lud@localhost.localdomain>
2016-08-14 18:54:27 +0100

    Added ibuffer-vc

--- init.el
+++ init.el
@@ -198,6 +198,15 @@
 (use-package yaml-mode
   :ensure t)
 
+(use-package ibuffer-vc
+  :ensure t
+  :config
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-vc-set-filter-groups-by-vc-root)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic)))))
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 (blink-cursor-mode -1)
