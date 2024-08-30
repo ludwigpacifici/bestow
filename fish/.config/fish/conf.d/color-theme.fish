0944ef10cfd6f40534416cfedf1553a4943e201c
Ludwig Pacifici <ludwig@lud.cc>
2024-08-30 15:34:12 +0100

    Added fish function to swap alacritty color theme

---
+++ fish/.config/fish/conf.d/color-theme.fish
@@ -0,0 +1,7 @@
+function solarized-dark
+    alacritty msg config "$(cat ~/.config/alacritty/solarized_dark.toml)"
+end
+
+function solarized-light
+    alacritty msg config "$(cat ~/.config/alacritty/solarized_light.toml)"
+end
